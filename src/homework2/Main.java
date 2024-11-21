f6473224a36794f0f69171850a6bae8bc846352b
Henry <felstedh@wlhs.wlwv.k12.or.us>
2024-11-20 20:54:31 -0800

    finished lol

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -1,10 +1,11 @@
 package homework2;
+import java.lang.Character;
 
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         vowelCounter("The quick brown fox jumped over the lazy dog.");
         System.out.println();
         vowelCounter("aaeeeeiiiioouuuu");
@@ -25,10 +26,61 @@ public class Main {
     }
 
     public static void vowelCounter(String str) {
+        //initialize the variables for counting vowels
+        int numA = 0;
+        int numE = 0;
+        int numI = 0;
+        int numO = 0;
+        int numU = 0;
+        //the selected character in the loop will be converted to lowercase and manually check if it is one of the 5 vowels
+        for (char letter : str.toCharArray()) {
 
+            if (Character.toLowerCase(letter) == 'a') {
+                numA++;
+            } else if (Character.toLowerCase(letter) == 'e') {
+                numE++;
+            } else if (Character.toLowerCase(letter) == 'i') {
+                numI++;
+            } else if (Character.toLowerCase(letter) == 'o') {
+                numO++;
+            } else if (Character.toLowerCase(letter) == 'u') {
+                numU++;
+            }
+        }
+        //print the results!
+        System.out.println("Number of a's: " + numA);
+        System.out.println("Number of e's: " + numE);
+        System.out.println("Number of i's: " + numI);
+        System.out.println("Number of o's: " + numO);
+        System.out.println("Number of u's: " + numU);
     }
 
     public static void palindromeChecker(String str) {
-
+        //initializes a variable to compare the character in the loop to
+        int compareTo = str.length()-1;
+        for (char letter : str.toCharArray()) {
+            //if the selected character from the loop does not equal its opposite counterpart
+            //it will check for an invalid, print, and break
+            if (Character.toLowerCase(letter) != (str.toLowerCase()).charAt(compareTo)) {
+                if (!Character.isLetter(letter)) {
+                    System.out.println(str.toLowerCase() + " has an invalid character");
+                    break;
+                }
+                System.out.println(str.toLowerCase() + " is not a palindrome.");
+                break;
+            } else {
+                //iterates the compared character variable and checks for both an invalid character
+                //and if it has finished iterating it will print
+                compareTo--;
+                if (!Character.isLetter(letter)) {
+                    System.out.println(str.toLowerCase() + " has an invalid character");
+                    break;
+                }
+                if (compareTo == 0) {
+                    System.out.println(str.toLowerCase()+" is a palindrome.");
+                    break;
+                }
+            }
+        }
     }
 }
